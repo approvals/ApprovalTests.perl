03b19d0d07189aaa7188986e410ae1b111c03c30
Jim Counts <jamesrcounts@outlook.com>
2013-02-26 06:35:33 -0800

    Module support files and cleanup

--- lib/Test/Approvals.pm
+++ lib/Test/Approvals.pm
@@ -1,5 +1,11 @@
 #! perl
+
+use strict;
+use warnings FATAL => 'all';
+
 package Test::Approvals;
 {
-
+    use version; our $VERSION = qv(0.0.1);
 }
+
+1;
--- lib/Test/Approvals/Reporters/Reporter.pm
+++ lib/Test/Approvals/Reporters/Reporter.pm
@@ -1,5 +1,11 @@
+#! perl
+
+use strict;
+use warnings FATAL => 'all';
+
 package Test::Approvals::Reporters::Reporter;
 {
+    use version; our $VERSION = qv(0.0.1);
     use Moose::Role;
 
     has test_name => ( is => 'rw', isa => 'Str', default => q{} );
