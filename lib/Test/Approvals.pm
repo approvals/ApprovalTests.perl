7f44a61c519074c622698f611ee77fd7f0eda695
James Counts <fritzjob18@nethere.com>
2013-04-09 21:08:37 -0700

    Approvals implementation

--- lib/Test/Approvals.pm
+++ lib/Test/Approvals.pm
@@ -6,6 +6,47 @@ use warnings FATAL => 'all';
 package Test::Approvals;
 {
     use version; our $VERSION = qv(0.0.1);
+    use Test::Approvals::Reporters;
+    use Test::Approvals::Namers::DefaultNamer;
+    use Test::Approvals::Core::FileApprover qw();
+    use Test::Approvals::Writers::TextWriter;
+
+    require Exporter;
+    use base qw(Exporter);
+
+    our @EXPORT_OK = qw(use_reporter verify reporter use_name namer);
+
+    my $namer_instance;
+    my $reporter_instance;
+
+    sub namer {
+        return $namer_instance;
+    }
+
+    sub reporter {
+        return $reporter_instance;
+    }
+
+    sub use_name {
+        my ($test_name) = @_;
+        $namer_instance =
+          Test::Approvals::Namers::DefaultNamer->new( name => $test_name );
+        return $namer_instance;
+    }
+
+    sub use_reporter {
+        my ($reporter_type) = @_;
+        $reporter_instance = $reporter_type->new();
+        return $reporter_instance;
+    }
+
+    sub verify {
+        my ($results) = @_;
+        my $w = Test::Approvals::Writers::TextWriter->new( result => $results );
+
+        return Test::Approvals::Core::FileApprover::verify( $w, namer(),
+            reporter() );
+    }
 }
 
 1;
---
+++ t/approvals.t
@@ -0,0 +1,23 @@
+#! perl
+use strict;
+use warnings FATAL => qw(all);
+use autodie;
+use version; our $VERSION = qv(0.0.1);
+
+use Test::Approvals::Specs qw(describe it run_tests);
+use Test::Approvals qw(verify use_reporter reporter use_name namer);
+use Test::More;
+
+describe 'An Approval', sub {
+    it 'Reports failure using the configured reporter', sub {
+        my ($spec) = @_;
+
+        use_reporter('Test::Approvals::Reporters::FakeReporter');
+        use_name($spec);
+        verify('Hello');
+        ok reporter()->was_called, $spec;
+        unlink namer()->get_received_file('txt');
+    };
+};
+
+run_tests();
