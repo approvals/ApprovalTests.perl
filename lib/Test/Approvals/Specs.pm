3f13dee35ffaa308563be3917818f63688a94261
Jim Counts <jamesrcounts@outlook.com>
2013-04-17 10:33:36 -0700

    Docs and Cleanup

--- lib/Test/Approvals/Specs.pm
+++ lib/Test/Approvals/Specs.pm
@@ -1,8 +1,7 @@
-#! perl
+package Test::Approvals::Specs;
+
 use strict;
 use warnings FATAL => qw(all);
-
-package Test::Approvals::Specs;
 
 use version; our $VERSION = qv(0.0.1);
 use base qw(Exporter);
@@ -44,7 +43,44 @@ __END__
 
 Test::Approvals::Specs - Tiny BDD Tools
 
-=head1 SUBROUTINES
+=head1 VERSION
+
+This documentation refers to Test::Approvals::Specs version 0.0.1
+
+=head1 SYNOPSIS
+
+    use Test::Approvals::Specs qw(describe it run_tests);
+    use Test::More;
+    use Bank::Account;
+
+    describe 'A bank account' => sub {
+        it 'Holds money' => sub {
+            my $name = shift;
+            my $bank_account = Bank::Account->new();
+            $bank_account->deposit(1, 'Bitcoin');
+            is $bank_account->balance, 1, $name;
+        };
+    };
+
+    run_tests();
+
+=head1 DESCRIPTION
+
+Test::Approvals::Specs represents the minimal subset of BDD testing tools I 
+needed to comfertably write the type of tests I like to write.  These methods 
+are generally written in the rspec spirit, but I'm not an expert in rspec style
+BDD.  I enjoy writing jasmine tests in js (http://pivotal.github.io/jasmine/) 
+and using igloo with C++ (http://igloo-testing.org/), and I wanted to bring the
+parts I liked to Perl.  There are a couple modules that do something similar 
+already, but I really wanted to avoid typing the test name twice, so I created 
+a set of tools that pass the name to the executable example.
+
+This is a very minimal approach and probably doesn't support features you might
+like, such as 'before_each' and 'before_context', spies, expectations, etc.  I 
+didn't need them!  If I ever need them I might add them.  If you need them I'd 
+be interested in hearing about it, maybe we can work together to add them.
+
+=head1 SUBROUTINES/METHODS
 
 =head2 describe
     
@@ -121,3 +157,52 @@ While this can be useful when migrating legacy tests, I don't reccomend it.
 'plan' is my least favorite aspect of the Perl test ecosystem, so I try to 
 avoid planning by always using examples, and always using one assertion per 
 example.  However, this functionality is there if you need it.
+
+=head1 DIAGNOSTICS
+
+None at this time.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+None.
+
+=head1 DEPENDENCIES
+
+=over
+
+Exporter
+Readonly
+Test::Builder
+version
+
+=back
+
+=head1 INCOMPATIBILITIES
+
+None known.
+
+=head1 BUGS AND LIMITATIONS
+
+Windows-only.  Linux/OSX/other support will be added when time and access to 
+those platforms permit.
+
+=head1 AUTHOR
+
+Jim Counts - @jamesrcounts
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2013 Jim Counts
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    L<http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
