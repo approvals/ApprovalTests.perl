c5ab3c8770d65d803cd249489d7715695a501269
Jim Counts <jamesrcounts@outlook.com>
2013-04-22 08:15:42 -0700

    Docs and Cleanup

--- lib/Test/Approvals/Reporters/DiffReporter.pm
+++ lib/Test/Approvals/Reporters/DiffReporter.pm
@@ -1,11 +1,10 @@
-#!perl
+package Test::Approvals::Reporters::DiffReporter;
 
 use strict;
 use warnings FATAL => 'all';
+use version; our $VERSION = qv('v0.0.1_2');
 
-package Test::Approvals::Reporters::DiffReporter;
 {
-    use version; our $VERSION = qv('v0.0.1_1');
     use Moose;
     use Test::Approvals::Reporters;
 
@@ -53,10 +52,76 @@ __PACKAGE__->meta->make_immutable;
 __END__
 =head1 NAME
 
-Test::Approvals::Reporters::DiffReporter - A reporter configured to choose 
-the first working diff utility it can find and use it for reporting.
+Test::Approvals::Reporters::DiffReporter 
+
+=head1 VERSION
+
+This documentation refers to Test::Approvals::Reporters::DiffReporter version v0.0.1_2
+
+=head1 SYNOPSIS
+
+    use Test::Approvals qw(verify_ok use_reporter);
+    use Test::Approvals::Reporters;
+    use Test::More;
+
+    use_reporter('Test::Approvals::Reporters::DiffReporter');
+    verify_ok 'Hello World', 'Hello Test';
+
+=head1 DESCRIPTION
+
+A MultiReporter configured to choose the first working diff utility it can find 
+and use it for reporting.
+
+=head1 SUBROUTINES/METHODS
 
 =head2 BUILD
 
 Used internally to configure a FirstWorkingReporter with instances of available
 diff Reporters.
+
+=head1 DIAGNOSTICS
+
+None at this time.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+None
+
+=head1 DEPENDENCIES
+
+=over 4
+
+Moose
+version
+
+=back
+
+=head1 INCOMPATIBILITIES
+
+None known.
+
+=head1 BUGS AND LIMITATIONS
+
+Windows-only.  Linux/OSX/other support will be added when time and access to 
+those platforms permit.
+
+=head1 AUTHOR
+
+Jim Counts - @jamesrcounts
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2013 Jim Counts
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    L<http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
